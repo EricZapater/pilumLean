90cbde2897555fc170ab3b04c5ce3df688f08568
Eric Zapater <ezapaterm@gmail.com>
2018-08-01 07:29:36 +0200

    Commit per publicar a vps

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+type Profile struct {
+	Name    string
+	Hobbies []string
+}
+
+type Kpi struct {
+	Descripcio string
+	Objectiu   float32
+	Valor      float32
+}
+
+const (
+	host     = "vps.pilumsoft.ovh"
+	port     = 5432
+	user     = "postgres"
+	password = "'En un lugar de la *'"
+	dbname   = "lean_dev"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/kpi", kpiHander)
+	http.ListenAndServe(":3000", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func kpiHander(w http.ResponseWriter, r *http.Request) {
+	kpi := Kpi{"OEE", 88.0, 88.1}
+	connStr := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	jskpi, err := json.Marshal(kpi)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jskpi)
+}
